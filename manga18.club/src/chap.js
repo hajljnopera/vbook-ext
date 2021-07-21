b874143c7072a1620a8d92ccfdd2d6f34edb263c
hajljnopera <39905585+hajljnopera@users.noreply.github.com>
2021-07-21 20:31:56 +0700

    update

--- manga18.club/src/chap.js
+++ manga18.club/src/chap.js
@@ -1,13 +1,27 @@
 load('libs.js');
+load('base64.js');
 
 function execute(url) {
-    var doc = Http.get(url).html();
-    var elems = $.QA(doc, '.chapter_boxImages img');
-    if (!elems.length) return Response.error(url);
-    
-    var imgs = elems.map(function(e){
-        return e.attr('src') || '';
-    });
-    
-    return Response.success(imgs);
+    var src = Http.get(url).string();
+
+    var slides_p_path = src.match(/var slides_p_path = \[(.*?)\]/);
+    if (slides_p_path && slides_p_path[1]) {
+        var imgArr = extractAllText(slides_p_path[1]);
+
+        if (imgArr.length > 0) {
+            return Response.success(imgArr.map(x => new Base64().decode(x)));
+        }
+    }
+
+    return Response.error(url);
+}
+
+function extractAllText(str) {
+    var re = /"(.*?)"/g;
+    var result = [];
+    var current;
+    while (current = re.exec(str)) {
+        result.push(current.pop());
+    }
+    return result;
 }
