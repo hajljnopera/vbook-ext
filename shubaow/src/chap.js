1492981f600beb66183cd5551d1becb8aebe87c8
hajljnopera <39905585+hajljnopera@users.noreply.github.com>
2021-11-28 21:51:34 +0700

    update js
    update shubaow

--- shubaow/src/chap.js
+++ shubaow/src/chap.js
@@ -3,10 +3,16 @@ load('libs.js');
 function execute(url) {
     // https://m.shubaow.net/104/104641 ---> https://www.shubaow.net/104_104641
     url = url.replace(/m\.shubaow\.net\/(\d+)\/(\d+)\/?/g, 'www.shubaow.net/$1_$2/');
-    var doc = Http.get(url).html('gbk');
-    var htm = $.Q(doc, '#content').html();
 
-    htm = cleanHtml(htm);
+    var response = fetch(url);
+    if (response.ok) {
+        var doc = response.html('gbk');
 
-    return Response.success(htm);
+        var htm = $.Q(doc, '#content').html();
+
+        htm = cleanHtml(htm);
+
+        return Response.success(htm);
+    }
+    return null;
 }
--- shubaow/src/detail.js
+++ shubaow/src/detail.js
@@ -3,20 +3,26 @@ load('libs.js');
 function execute(url) {
     var host = 'https://www.shubaow.net';
     url = url.replace(/(www|m)\.shubaow\.net\/(\d+)[_\/](\d+)\/?$/, 'www.shubaow.net/$2_$3/').append('/');
-    var doc = Http.get(url).html('gbk');
 
-    var coverImg = $.Q(doc, '#fmimg img').attr('src').mayBeFillHost(host);
-    var author = $.Q(doc, '#info p').text().normalizeCN().replace(/作\s*者:/g, '').trim();
-    var lastUpdated = $.Q(doc, '#info > p:nth-child(4)').text().normalizeCN().replace('发表时间:', '').trim();
+    var response = fetch(url);
+    if (response.ok) {
+        var doc = response.html('gbk');
 
-    return Response.success({
-        name: $.Q(doc, '#info h1').text(),
-        cover: coverImg,
-        author: author,
-        description: $.Q(doc, '#intro').text(),
-        detail: String.format('作者: {0}<br>最后更新: {1}', author, lastUpdated),
-        host: host
-    });
+        var coverImg = $.Q(doc, '#fmimg img').attr('src').mayBeFillHost(host);
+        var author = $.Q(doc, '#info p').text().normalizeCN().replace(/作\s*者:/g, '').trim();
+        var lastUpdated = $.Q(doc, '#info > p:nth-child(4)').text().normalizeCN().replace('发表时间:', '').trim();
+
+        return Response.success({
+            url: url.append('/'),
+            name: $.Q(doc, '#info h1').text(),
+            cover: coverImg,
+            author: author,
+            description: $.Q(doc, '#intro').text(),
+            detail: String.format('作者: {0}<br>最后更新: {1}', author, lastUpdated),
+            host: host
+        });
+    }
+    return null;
 }
 
 String.prototype.normalizeCN = function() {
