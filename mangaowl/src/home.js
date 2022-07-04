4abbfb796ee199ca64691975cdaf6aac67484c5b
hajljnopera <39905585+hajljnopera@users.noreply.github.com>
2022-07-04 10:14:23 +0700

    Removing dead sources

--- mangaowl/src/home.js
+++
@@ -1,7 +0,0 @@
-function execute() {
-    return Response.success([
-        {title: "Lastest", input: "/lastest/{0}", script: "gen.js"},
-        {title: "Popular", input: "/popular/{0}", script: "gen.js"},
-        {title: "New Release", input: "/new_release/{0}", script: "gen.js"},
-    ]);
-}
--- nofff/src/chap.js
+++
@@ -1,9 +0,0 @@
-load('libs.js');
-
-function execute(url) {
-    url = url.replace('m.nofff.com', 'www.nofff.com');
-    var doc = Http.get(url).html('gbk');
-    var htm = $.Q(doc, '#content').html();
-
-    return Response.success(htm);
-}
